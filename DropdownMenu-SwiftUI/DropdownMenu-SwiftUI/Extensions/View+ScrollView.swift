151be7b00e114e9965f1a59ec605788056e461f2
Marlon Raskin <marlonr@hey.com>
2023-04-08 17:04:51 -0400

    Adds starter code and files

---
+++ DropdownMenu-SwiftUI/DropdownMenu-SwiftUI/Extensions/View+ScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  View+ScrollView.swift
+//  DropdownMenu-SwiftUI
+//
+//  Created by Marlon Raskin on 2023-04-08.
+//
+
+import SwiftUI
+
+extension View {
+    /// Embeds the view inside of a `ScrollView` based on a condition
+    @ViewBuilder func embedInScrollView(
+        _ shouldEmbed: Bool = true,
+        axis: Axis.Set = .vertical,
+        showsIndicators: Bool = true
+    ) -> some View {
+        if shouldEmbed {
+            ScrollView(axis, showsIndicators: showsIndicators) { self }
+        } else { self }
+    }
+}
---
+++ DropdownMenu-SwiftUI/DropdownMenu-SwiftUI/Modifiers & Styles/SmoothRoundedCorners.swift
@@ -0,0 +1,35 @@
+//
+//  SmoothRoundedCorners.swift
+//  DropdownMenu-SwiftUI
+//
+//  Created by Marlon Raskin on 2023-04-08.
+//
+
+import SwiftUI
+
+/// Clips the view to its bounding frame with specified corner radius and curve style.
+struct CornerRadius: ViewModifier {
+    let cornerRadius: CGFloat
+    let style: RoundedCornerStyle
+    
+    init(_ cornerRadius: CGFloat, style: RoundedCornerStyle = .continuous) {
+        self.cornerRadius = cornerRadius
+        self.style = style
+    }
+    
+    func body(content: Content) -> some View {
+        content.clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: style))
+    }
+}
+
+extension View {
+    /// Clips the view to its bounding frame with specified corner radius. Uses the `continuous` style.
+    func smoothRoundedCorners(_ cornerRadius: CGFloat = 10.0) -> some View {
+        modifier(CornerRadius(cornerRadius, style: .continuous))
+    }
+    
+    /// Clips the view to its bounding frame with specified corner radius. Uses the `circular` (default) style.
+    func roundedCorners(_ cornerRadius: CGFloat = 10.0) -> some View {
+        modifier(CornerRadius(cornerRadius, style: .circular))
+    }
+}
